56f13c5dc526ec42f84ade24d2ca21fe6adc6f2c
jday151r <j.day151r@gmail.com>
2019-11-13 17:42:21 -0800

    initial project files, panning and zooming]

---
+++ Assets/Scripts/ViewControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach directly to Camera object!
+public class ViewControls : MonoBehaviour
+{
+    public Camera cam;
+    public Vector2 orthoSizeRange;
+    [Range(0, 1)] public float zoomSpeed;
+
+    Vector3 mouseDragStart;
+    
+    void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize -= cam.orthographicSize * zoomSpeed, orthoSizeRange.x, orthoSizeRange.y);
+        if (Input.mouseScrollDelta.y < 0)
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize += cam.orthographicSize * zoomSpeed, orthoSizeRange.x, orthoSizeRange.y);
+        if (!Input.GetMouseButton(2))
+        {
+            mouseDragStart = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(2))
+        {
+            Vector2 difference = mouseDragStart - cam.ScreenToWorldPoint(Input.mousePosition);
+            transform.Translate(difference.x, difference.y, 0);
+            mouseDragStart = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+}
